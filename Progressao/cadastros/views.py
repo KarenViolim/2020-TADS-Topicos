ea29b988862e26daf0e47a5649616eb4b55c63e0
KarenViolim <karen.violim2011@gmail.com>
2020-09-23 21:05:12 -0300

    Criado links na index e finalizado opção de editar e excluir.

--- Progressao/cadastros/views.py
+++ Progressao/cadastros/views.py
@@ -15,31 +15,31 @@ class GrupoCreate(CreateView):
     model = Grupo
     fields = ['descricao']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-grupo')
 
 class EquipamentoCreate(CreateView):
     model = Equipamento
     fields = ['nome_eq','fornecedor','marca','grupo','valor_compra','valor_venda']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-equipamento')
 
 class ServicoCreate(CreateView):
     model = Servico
     fields = ['descricao','tipo_servico','valor']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-servico')
 
 class ClienteCreate(CreateView):
     model = Cliente
     fields = ['nome_cliente','documento','cep','endereco','numero','bairro','cidade','celular','email']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-cliente')
 
 class FuncionarioCreate(CreateView):
     model = Funcionario
     fields = ['nome_funcionario','cep','endereco','numero','bairro','cidade','celular','email','cargo']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-funcionario')
 
 ########## UPDATES ########## 
 class MarcaUpdate(UpdateView):
@@ -52,31 +52,31 @@ class GrupoUpdate(UpdateView):
     model = Grupo
     fields = ['descricao']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-grupo')
 
 class EquipamentoUpdate(UpdateView):
     model = Equipamento
     fields = ['nome_eq','fornecedor','marca','grupo','valor_compra','valor_venda']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-equipamento')
 
 class ServicoUpdate(UpdateView):
     model = Servico
     fields = ['descricao','tipo_servico','valor']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-servico')
 
 class ClienteUpdate(UpdateView):
     model = Cliente
     fields = ['nome_cliente','documento','cep','endereco','numero','bairro','cidade','celular','email']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-cliente')
 
 class FuncionarioUpdate(UpdateView):
     model = Funcionario
     fields = ['nome_funcionario','cep','endereco','numero','bairro','cidade','celular','email','cargo']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-funcionario')
 
 ########## DELETE ########## 
 class MarcaDelete(DeleteView):
@@ -87,27 +87,27 @@ class MarcaDelete(DeleteView):
 class GrupoDelete(DeleteView):
     model = Grupo
     template_name = 'cadastros/form-excluir.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-grupo')
 
 class EquipamentoDelete(DeleteView):
     model = Equipamento
     template_name = 'cadastros/form-excluir.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-equipamento')
 
 class ServicoDelete(DeleteView):
     model = Servico
     template_name = 'cadastros/form-excluir.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-servico')
 
 class ClienteDelete(DeleteView):
     model = Cliente
     template_name = 'cadastros/form-excluir.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-cliente')
 
 class FuncionarioDelete(DeleteView):
     model = Funcionario
     template_name = 'cadastros/form-excluir.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-funcionario')
 
 ########## LISTA ########## 
 class MarcaList(ListView):
